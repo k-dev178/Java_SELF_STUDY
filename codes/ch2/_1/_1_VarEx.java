27d2efc51003ced625cba82ffbda9d3cc0b1cc2c
Kim_SJ <107867776+k-dev178@users.noreply.github.com>
2025-10-24 02:32:10 +0900

    [add]Java.ch2._1 Ex codes

---
+++ codes/ch2/_1/_1_VarEx.java
@@ -0,0 +1,15 @@
+public class _1_VarEx {
+    public static void main(String[] args) {
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.print(year);
+        System.out.print(age);
+    }
+}
---
+++ codes/ch2/_1/_2_VarEx2.java
@@ -0,0 +1,14 @@
+class _2_VarEx2{
+    public static void main(String[] agrs){
+        int x = 10, y = 20;
+        int tmp = 0;
+
+        System.out.println("x:" + x + " y:" + y);
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("x:" + x + " y:" + y);
+    }
+}
